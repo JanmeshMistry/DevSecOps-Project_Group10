5347f7564ff6cbdf76fb021a84ff9c9d7ec1e984
JanmeshMistry <janmesh008@gmail.com>
2026-05-11 13:57:08 -0400

    Added HTTP security headers for enhanced protection

--- app/app.py
+++ app/app.py
@@ -8,9 +8,20 @@ products = [
     {"name": "Headphones", "price": "₹3,000"}
 ]
 
+@app.after_request
+def add_security_headers(response):
+
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['X-Frame-Options'] = 'DENY'
+    response.headers['X-XSS-Protection'] = '1; mode=block'
+
+    return response
+
+
 @app.route('/')
 def home():
     return render_template("index.html", products=products)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
